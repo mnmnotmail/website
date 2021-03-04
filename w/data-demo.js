6324e53f1e91a3ed4f71bc98a9f131a71918858e
Liam <github@networkimprov.net>
2021-03-03 22:32:09 -0800

    demo: Add /w/data-demo.js (pr9)

---
+++ w/data-demo.js
@@ -0,0 +1,192 @@
+// Copyright 2021 Liam Breck
+// Published at https://github.com/networkimprov/mnm-hammer
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+mnm.demoData = {
+      '/v': [{"Name":"Blue","NoticeN":1,"UnreadN":1},
+             {"Name":"Gold","NoticeN":0,"UnreadN":0}] ,
+
+      '/t': [{"Name":"logo-400-inset-bright.png","Size":70070,"Date":"2019-10-24T21:26:57Z"},
+             {"Name":"logo-400-bleed-dim.png","Size":103533,"Date":"2019-10-24T20:57:53Z"},
+             {"Name":"BlueFile","Size":26,"Date":"2019-05-29T06:33:20Z"},
+             {"Name":"Gold/File","Size":26,"Date":"2019-05-29T06:33:20Z"}] ,
+
+      '/f': [{"Name":"Icecream","Spec":true,"Revs":[
+              {"Id":"1","Date":"2019-10-29T22:39:24Z"},
+              {"Id":"spec","Date":"2019-10-29T04:35:47Z"}] }] ,
+
+      '/g': [{"Id":"2020-05-06T02:56:08Z","Name":"mnm notes"},
+             {"Id":"Todo","Name":"Todo"}] ,
+
+      '/l': {"Addr":"192.168.1.2:8123","Pin":""} ,
+
+      S: {Blue: {
+             A:  [["Gold 0528233319", "Y5Z%3GSZDVKK1BLPK1DHY4TZ128T18DX"]] ,
+
+             cs: {"Sort":{"cl":"Who","al":"Date","t":"Date","f":"Date"},
+                  "Thread":"1647c7fe052c8e6d",
+                  "ThreadTabs":{"Pos":0,"PosFor":0,"Terms":[],"Type":0},
+                  "History":{"Prev":false,"Next":false},
+                  "SvcTabs":{"Pos":0,"PosFor":0,"Terms":[],"Pinned":[],"Type":1}} ,
+
+             cf: {"Name":"Blue","HistoryLen":88,"LoginPeriod":0,"Addr":"=192.168.1.3:443",
+                  "Verify":false,"Alias":"Blue 0528233319","Uid":"5H7QC1XG1+LP++6PLW9RPBY3N%CRCFV6",
+                  "NodeSet":[{"Name":"first","Status":97,"Local":true}]} ,
+
+             cn: {"Addr":"","Pin":"","Xfer":0} ,
+
+             ps: [] ,
+
+             pt: [{"Type":3,"Date":"2020-04-22T04:53:48.395Z",
+                   "Text":"123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789.",
+                   "Alias":"Gold 0528233319","MyAlias":"Blue 0528233319","Response":{}},
+                  {"Type":3,"Date":"2019-05-29T06:33:35.309Z",
+                   "Alias":"Gold 0528233319","MyAlias":"Blue 0528233319",
+                   "Response":{
+                    "Type":8,"Date":"2019-05-29T06:33:36.063Z","Alias":"Gold 0528233319",
+                    "Uid":"Y5Z%3GSZDVKK1BLPK1DHY4TZ128T18DX","MyAlias":"Blue 0528233319",
+                    "MsgId":"15a30e173fa0c452","Gid":"Gold-G 0528233319",
+                    "Qid":"Gold-G 0528233319_016b024b0fd9","Response":{} }}] ,
+
+             pf: [{"Type":4,"Date":"2020-05-31T17:05:36.284Z",
+                   "Text":"test post server filename change",
+                   "Alias":"Gold 0528233319","Uid":"Y5Z%3GSZDVKK1BLPK1DHY4TZ128T18DX",
+                   "MyAlias":"Blue 0528233319","MsgId":"16142b12a75ec3b2","Response":{} },
+                  {"Type":8,"Date":"2019-05-29T06:33:36.063Z",
+                   "Alias":"Gold 0528233319","Uid":"Y5Z%3GSZDVKK1BLPK1DHY4TZ128T18DX",
+                   "MyAlias":"Blue 0528233319","MsgId":"15a30e173fa0c452","Gid":"Gold-G 0528233319",
+                   "Response":{},"ResponseInvt":{
+                    "Type":9,"Date":"2019-05-29T06:33:36.706Z",
+                    "Gid":"Gold-G 0528233319","Response":{} }}] ,
+
+             gl: [{"Gid":"Gold-G 0528233319","Date":"2019-05-29T06:33:36.706Z","Admin":false}] ,
+
+             ot: [] ,
+
+             of: [{"Date":"2021-01-24T22:27:48Z","Uid":"Y5Z%3GSZDVKK1BLPK1DHY4TZ128T18DX",
+                   "Alias":"Gold 0528233319"}] ,
+
+             nl: [{"Type":"i","MsgId":"16142b12a75ec3b2","Date":"2020-05-31T17:08:16Z","Seen":0,"Alias":"Gold 0528233319","Blurb":"test post server filename change"},
+                  {"Type":"i","MsgId":"6","Date":"2019-05-28T06:33:29Z","Seen":4,"Alias":"seen"},
+                  {"Type":"i","MsgId":"5","Date":"2019-05-28T06:33:29Z","Seen":4,"Alias":"seen"}] ,
+
+             fl: [{"Id":"networkimprov.github.io/mnm-registry/first_recv","Date":"2019-05-29T06:33:28Z"},
+                  {"Id":"networkimprov.github.io/mnm-registry/first_sent","Date":"2019-05-29T06:33:23Z"}] ,
+
+             tl: [{"Id":"1647c7fe052c8e6d","Count":2,"Subject":"testing delays","Unread":true,
+                   "OrigCc":["Gold 0528233319"],
+                   "OrigDate":"2020-11-15T20:25:17.157Z","LastDate":"2020-11-15T20:26:58.960Z",
+                   "OrigAuthor":"Blue 0528233319","LastAuthor":"Gold 0528233319"},
+                  {"Id":"15a30e173fa0c457","Count":11,"Subject":"reply ohi","SubjectWas":"ohi",
+                   "OrigCc":["Blue 0528233319"],
+                   "OrigDate":"2019-05-29T06:33:37.006Z","LastDate":"2020-11-15T20:21:33.085Z",
+                   "OrigAuthor":"Blue 0528233319","LastAuthor":"Blue 0528233319"},],
+             tx: [{"Id":"_016b024c7088","Count":0,"Subject":"abc","OrigCc":[],
+                   "OrigDate":"2019-05-29T06:40:00Z","LastDate":"2019-05-29T06:40:00Z",
+                   "OrigAuthor":"Blue 0528233319","LastAuthor":""},
+                  {"Id":"15a30e173fa0c459","Count":2,"Subject":"to forward","OrigCc":[],
+                   "OrigDate":"2019-05-29T06:33:40.076Z","LastDate":"2019-05-29T06:33:41.229Z",
+                   "OrigAuthor":"Gold 0528233319","LastAuthor":"Gold 0528233319"}] ,
+
+             T:  {"1647c7fe052c8e6d": {
+                  ml: [{"Id":"1647c847245648f6","From":"Y5Z%3GSZDVKK1BLPK1DHY4TZ128T18DX",
+                        "Alias":"Gold 0528233319","Date":"2020-11-15T20:26:58.960Z",
+                        "Subject":"",
+                        "Seen":"","Queued":false},
+                       {"Id":"1647c847245648f5","From":"Y5Z%3GSZDVKK1BLPK1DHY4TZ128T18DX",
+                        "Alias":"Gold 0528233319","Date":"2020-11-15T20:26:58.960Z",
+                        "Subject":"",
+                        "Seen":"2020-11-15T20:27:38Z","Queued":false},
+                       {"Id":"1647c7fe052c8e6d","From":"5H7QC1XG1+LP++6PLW9RPBY3N%CRCFV6",
+                        "Alias":"Blue 0528233319","Date":"2020-11-15T20:25:17.157Z",
+                        "Subject":"testing delays",
+                        "Seen":".","Tags":["Todo"],"Queued":false}] ,
+
+                  mo: {"1647c7fe052c8e6d":
+                       {"Id":"1647c7fe052c8e6d","Len":5,"Size":5,
+                        "Posted":"2020-11-15T20:25:17.157Z",
+                        "From":"5H7QC1XG1+LP++6PLW9RPBY3N%CRCFV6",
+                        "SubHead":{"Alias":"Blue 0528233319","ThreadId":"1647c7fe052c8e6d",
+                                   "Subject":"testing delays"},
+                        "msg_data":"boink"},
+                       "1647c847245648f5":
+                       {"Id":"1647c847245648f5","Len":2,"Size":2,
+                        "Posted":"2020-11-15T20:26:58.960Z",
+                        "From":"Y5Z%3GSZDVKK1BLPK1DHY4TZ128T18DX",
+                        "SubHead":{"Alias":"Gold 0528233319","ThreadId":"1647c7fe052c8e6d",
+                                   "Subject":""},
+                        "msg_data":"ok"}} ,
+
+                  cl: [[],
+                       [{"Who":"Blue 0528233319","By":"Blue 0528233319",
+                         "WhoUid":"5H7QC1XG1+LP++6PLW9RPBY3N%CRCFV6","ByUid":"5H7QC1XG1+LP++6PLW9RPBY3N%CRCFV6",
+                         "Date":"2020-11-15T20:25:17.157Z","Note":"author","Subscribe":true,"Queued":false},
+                        {"Who":"Gold 0528233319","By":"Blue 0528233319",
+                         "WhoUid":"Y5Z%3GSZDVKK1BLPK1DHY4TZ128T18DX","ByUid":"5H7QC1XG1+LP++6PLW9RPBY3N%CRCFV6",
+                         "Date":"2020-11-15T20:25:17.157Z","Note":"","Subscribe":true,"Queued":false}]] ,
+
+                  al: [] },
+
+                  "15a30e173fa0c457": {
+                  ml: [{"Id":"1647c7fe052c8e67","From":"5H7QC1XG1+LP++6PLW9RPBY3N%CRCFV6","Alias":"Blue 0528233319","Date":"2020-11-15T20:21:33.085Z","Subject":"reply ohi","Seen":".","Queued":false},
+                       {"Id":"160a0f4621fc601d","From":"5H7QC1XG1+LP++6PLW9RPBY3N%CRCFV6","Alias":"Blue 0528233319","Date":"2020-04-29T19:11:27.119Z","Subject":"reply ohi","Seen":".","Queued":false},
+                       {"Id":"160a0f4621fc601c","From":"5H7QC1XG1+LP++6PLW9RPBY3N%CRCFV6","Alias":"Blue 0528233319","Date":"2020-04-29T19:10:57.529Z","Subject":"reply ohi","Seen":".","Queued":false},
+                       {"Id":"1609e70e45635abe","From":"5H7QC1XG1+LP++6PLW9RPBY3N%CRCFV6","Alias":"Blue 0528233319","Date":"2020-04-28T06:14:32.078Z","Subject":"reply ohi","Seen":".","Tags":["Todo"],"Queued":false},
+                       {"Id":"1609e6cdf061653a","From":"5H7QC1XG1+LP++6PLW9RPBY3N%CRCFV6","Alias":"Blue 0528233319","Date":"2020-04-28T06:09:48.343Z","Subject":"reply ohi","Seen":".","Queued":false},
+                       {"Id":"1609e61c58e6f761","From":"5H7QC1XG1+LP++6PLW9RPBY3N%CRCFV6","Alias":"Blue 0528233319","Date":"2020-04-28T05:57:03.943Z","Subject":"reply ohi","Seen":".","Queued":false},
+                       {"Id":"1609e4db0a54ee25","From":"5H7QC1XG1+LP++6PLW9RPBY3N%CRCFV6","Alias":"Blue 0528233319","Date":"2020-04-28T05:34:17.100Z","Subject":"reply ohi","Seen":".","Queued":false},
+                       {"Id":"1609e42510b0c265","From":"5H7QC1XG1+LP++6PLW9RPBY3N%CRCFV6","Alias":"Blue 0528233319","Date":"2020-04-28T05:21:34.931Z","Subject":"reply ohi","Seen":".","Queued":false},
+                       {"Id":"15c7f5aab2710d89","From":"5H7QC1XG1+LP++6PLW9RPBY3N%CRCFV6","Alias":"Blue 0528233319","Date":"2019-10-19T22:54:53.408Z","Subject":"reply ohi","Seen":".","Queued":false},
+                       {"Id":"15a30e173fa0c458","From":"Y5Z%3GSZDVKK1BLPK1DHY4TZ128T18DX","Alias":"Gold 0528233319","Date":"2019-05-29T06:33:39.296Z","Subject":"","Seen":"2019-05-29T06:34:44Z","Queued":false},
+                       {"Id":"15a30e173fa0c457","From":"5H7QC1XG1+LP++6PLW9RPBY3N%CRCFV6","Alias":"Blue 0528233319","Date":"2019-05-29T06:33:37.006Z","Subject":"ohi","Seen":".","Queued":false}] ,
+
+                  mo: {"15a30e173fa0c457":{"Id":"15a30e173fa0c457","Len":0,"Posted":"2019-05-29T06:33:37.006Z","From":"5H7QC1XG1+LP++6PLW9RPBY3N%CRCFV6","SubHead":{"Alias":"Blue 0528233319","ThreadId":"15a30e173fa0c457","Subject":"ohi","Attach":[{"Name":"u:BlueFile","Size":26},{"Name":"f:Blue.original","Size":402,"Ffn":"networkimprov.github.io/mnm-registry/first"},{"Name":"r:Blue.original","Size":33,"Ffn":"networkimprov.github.io/mnm-registry/first"}]},"msg_data":""},
+                       "15a30e173fa0c458":{"Id":"15a30e173fa0c458","Len":0,"Posted":"2019-05-29T06:33:39.296Z","From":"Y5Z%3GSZDVKK1BLPK1DHY4TZ128T18DX","SubHead":{"Alias":"Gold 0528233319","ThreadId":"15a30e173fa0c457","Subject":"","Attach":[{"Name":"r:Blue.original","Size":33,"Ffn":"networkimprov.github.io/mnm-registry/first"}]},"msg_data":""},
+                       "15c7f5aab2710d89":{"Id":"15c7f5aab2710d89","Len":0,"Posted":"2019-10-19T22:54:53.408Z","From":"5H7QC1XG1+LP++6PLW9RPBY3N%CRCFV6","SubHead":{"Alias":"Blue 0528233319","ThreadId":"15a30e173fa0c457","Subject":"reply ohi"},"msg_data":""},
+                       "1609e42510b0c265":{"Id":"1609e42510b0c265","Len":13,"Posted":"2020-04-28T05:21:34.931Z","From":"5H7QC1XG1+LP++6PLW9RPBY3N%CRCFV6","SubHead":{"Alias":"Blue 0528233319","ThreadId":"15a30e173fa0c457","Subject":"reply ohi","Attach":[{"Name":"u:mnm-attached.png","Size":15992}]},"msg_data":"sendfile test"},
+                       "1609e4db0a54ee25":{"Id":"1609e4db0a54ee25","Len":15,"Posted":"2020-04-28T05:34:17.100Z","From":"5H7QC1XG1+LP++6PLW9RPBY3N%CRCFV6","SubHead":{"Alias":"Blue 0528233319","ThreadId":"15a30e173fa0c457","Subject":"reply ohi","Attach":[{"Name":"u:mnm-attachlist-open.png","Size":8823}]},"msg_data":"sendfile retest"},
+                       "1609e61c58e6f761":{"Id":"1609e61c58e6f761","Len":14,"Posted":"2020-04-28T05:57:03.943Z","From":"5H7QC1XG1+LP++6PLW9RPBY3N%CRCFV6","SubHead":{"Alias":"Blue 0528233319","ThreadId":"15a30e173fa0c457","Subject":"reply ohi","Attach":[{"Name":"u:mnm-attachlist.png","Size":8730}]},"msg_data":"sendfile again"},
+                       "1609e6cdf061653a":{"Id":"1609e6cdf061653a","Len":8,"Posted":"2020-04-28T06:09:48.343Z","From":"5H7QC1XG1+LP++6PLW9RPBY3N%CRCFV6","SubHead":{"Alias":"Blue 0528233319","ThreadId":"15a30e173fa0c457","Subject":"reply ohi"},"msg_data":"yet more"},
+                       "1609e70e45635abe":{"Id":"1609e70e45635abe","Len":13,"Posted":"2020-04-28T06:14:32.078Z","From":"5H7QC1XG1+LP++6PLW9RPBY3N%CRCFV6","SubHead":{"Alias":"Blue 0528233319","ThreadId":"15a30e173fa0c457","Subject":"reply ohi"},"msg_data":"again n again"},
+                       "160a0f4621fc601c":{"Id":"160a0f4621fc601c","Len":10,"Posted":"2020-04-29T19:10:57.529Z","From":"5H7QC1XG1+LP++6PLW9RPBY3N%CRCFV6","SubHead":{"Alias":"Blue 0528233319","ThreadId":"15a30e173fa0c457","Subject":"reply ohi"},"msg_data":"1111111112"},
+                       "160a0f4621fc601d":{"Id":"160a0f4621fc601d","Len":4,"Posted":"2020-04-29T19:11:27.119Z","From":"5H7QC1XG1+LP++6PLW9RPBY3N%CRCFV6","SubHead":{"Alias":"Blue 0528233319","ThreadId":"15a30e173fa0c457","Subject":"reply ohi"},"msg_data":"1112"},
+                       "1647c7fe052c8e67":{"Id":"1647c7fe052c8e67","Len":16,"Size":16,"Posted":"2020-11-15T20:21:33.085Z","From":"5H7QC1XG1+LP++6PLW9RPBY3N%CRCFV6","SubHead":{"Alias":"Blue 0528233319","ThreadId":"15a30e173fa0c457","Subject":"reply ohi"},"msg_data":"try delayed send"}} ,
+
+                  cl: [[],
+                       [{"Who":"Blue 0528233319","By":"Blue 0528233319","WhoUid":"5H7QC1XG1+LP++6PLW9RPBY3N%CRCFV6","ByUid":"5H7QC1XG1+LP++6PLW9RPBY3N%CRCFV6","Date":"2020-11-15T20:25:17.157Z","Note":"author","Subscribe":true,"Queued":false},
+                        {"Who":"Gold 0528233319","By":"Blue 0528233319","WhoUid":"Y5Z%3GSZDVKK1BLPK1DHY4TZ128T18DX","ByUid":"5H7QC1XG1+LP++6PLW9RPBY3N%CRCFV6","Date":"2020-11-15T20:25:17.157Z","Note":"","Subscribe":true,"Queued":false}] ] ,
+
+                  al: [] }
+             } },
+
+          Gold: {
+             A:  [["Gold 0528233319", "Y5Z%3GSZDVKK1BLPK1DHY4TZ128T18DX"]] ,
+
+             cs: {"Sort":{"cl":"Who","al":"Date","t":"Date","f":"Date"},
+                  "Thread":"",
+                  "ThreadTabs":{"Pos":0,"PosFor":0,"Terms":[],"Type":0},
+                  "History":{"Prev":false,"Next":false},
+                  "SvcTabs":{"Pos":0,"PosFor":0,"Terms":[],"Pinned":[],"Type":1}} ,
+
+             cf: {"Name":"Gold","HistoryLen":88,"LoginPeriod":0,"Addr":"=192.168.1.3:443",
+                  "Verify":false,"Alias":"Gold 0528233319","Uid":"4H7QC1XG1+LP++6PLW9RPBY3N%CRCFV6",
+                  "NodeSet":[{"Name":"first","Status":97,"Local":true}]} ,
+
+             cn: {"Addr":"","Pin":"","Xfer":0} ,
+
+             ps: [] ,
+             pt: [] ,
+             pf: [] ,
+             gl: [] ,
+             ot: [] ,
+             of: [] ,
+             nl: [] ,
+             fl: [] ,
+             tl: [] ,
+
+             T:  {
+             } },
+      } };
