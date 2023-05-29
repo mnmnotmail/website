76790677490ccf204bfae1d450b7af1ce9050892
Liam <github@networkimprov.net>
2023-05-28 23:42:57 -0700

    brangr: Add update

--- bgr/brangr-config.js
+++ bgr/brangr-config.js
@@ -1,11 +1,8 @@
-/* Configuration - Brangr v0.2
-   For the source code and latest release, see https://github.com/networkimprov/brangr
-
-   Edit this file as appropriate for your GraphQL site, and rename it brangr_config.js
-*/
+// Demo config - Brangr v0.x
 
 export default  {
-   // Info for this GraphQL site; appears on Docs page
+   test: true,
+
    title: 'Demo',
    notes: 'A Brangr demo by its author. It calls public GraphQL services for \
            SpaceX info, the Star Wars API ("swapi"), and countries of the world. \
@@ -14,26 +11,15 @@ export default  {
            for schema queries if someone asks github.com/trevorblades nicely :)',
    url: 'https://github.com/networkimprov/brangr',
 
-   // One or more URLs to GraphQL endpoints; can be a string, array, or object
    targets: {
       spacex: 'https://spacex-production.up.railway.app/',
       swapi: 'https://swapi-graphql.netlify.app/.netlify/functions/index',
       countries: 'https://countries.trevorblades.com/graphql',
    },
-// targets: 'url',
-// targets: ['url', 'url2'],
-// targets: {one: 'url', two: 'url2'}, // provides CSS classes .site-one & .site-two
 
-   // Name of a query page to display when Brangr loads
    show: 'SpaceX',
-
-   // Name of a query page to display and run when Brangr loads (supersedes .show)
 // run: 'file',
 
-   // Set of query pages available when Brangr loads, with fields:
-   //   name - title of the query page
-   //   text - text of the query, or...
-   //   url  - text of the query is online at this address (supersedes .text)
    qs: [{
       name: 'SpaceX',
       site: 'spacex',
@@ -49,8 +35,6 @@ export default  {
       url:  './q-swapi.txt',
    }],
 
-   // Query that loads the GraphQL schema for any of the .targets URLs
-   // If needed, revise the fragment 'typeFields' to support longer ofType chains.
    schemaQ: '{ __schema {\
    ##description\n\
      queryType { name }\
